17e778cfee0a1b58c8abe1c2b2344c00c37c40d4
yanamyron <115184405+yanamyron@users.noreply.github.com>
2024-06-09 00:19:09 +0300

    Create index.js

---
+++ server/src/api/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import db from "./config/Database.js";
+import router from "./routes/index.js";
+dotenv.config();
+const app = express();
+app.use(cors({ credentials: true, origin: 'https://fliandrivka.vercel.app/' })); 
+app.use(cookieParser());
+app.use(express.json());
+app.use(router);
+app.listen(process.env.PORT || 5000, () => console.log('Server running at port', process.env.PORT || 5000));
