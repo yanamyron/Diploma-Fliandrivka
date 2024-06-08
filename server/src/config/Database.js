c34c6a2ce4f69d597daa5a882728825dd2436995
yanamyron <115184405+yanamyron@users.noreply.github.com>
2024-06-08 12:53:15 +0300

    Update Database.js

--- server/src/config/Database.js
+++ server/src/config/Database.js
@@ -1,6 +1,15 @@
 import { Sequelize } from "sequelize";
-const db = new Sequelize("db", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+
+// Використовуйте змінні середовища для підключення до бази даних
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT, // Вкажіть порт якщо відрізняється від стандартного
+  }
+);
+
 export default db;
